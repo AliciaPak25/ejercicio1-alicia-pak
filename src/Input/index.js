5f5e189c4ae85f454f0d5a7ef09a1042003a3908
AliciaPak25 <aliciappak@gmail.com>
2022-05-16 13:07:05 -0300

    ejercicio terminado

--- src/Input/index.js
+++ src/Input/index.js
@@ -1,38 +1,78 @@
-import React, { Component } from 'react'
+import React, {useState, useEffect} from 'react'
 import { getFakeData1, getFakeData2 } from '../fakeRequest'
 
-export default class Input extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      inputValueInComponent: ''
+export default function Input(props) {
+    const [inputValueInComponent, setinputValueInComponent] = useState('')
+    const [arrayNames, setArrayNames] = useState([])
+    const [arrayLastNames, setArrayLastNames] = useState([])
+
+    const getData = async() => {
+        const firstNameWithId = await getFakeData1()
+        const lastNameWithId = await getFakeData2()
+        
+        console.log({firstNameWithId, lastNameWithId})
+
+        setArrayNames(firstNameWithId)
+        setArrayLastNames(lastNameWithId)
     }
-  }
+    useEffect(()=>{
+        getData()
+        
+    },[])
+    
+    useEffect(()=>{
+        setinputValueInComponent(props.inputValue)
+    },[props.inputValue])
 
-  async componentDidMount() {
-    const firstNameWithId = await getFakeData1()
-    const lastNameWithId = await getFakeData2()
+    const arrayFullNames = arrayNames.concat(arrayLastNames);
+    
+    var sameIdCount = 0;
+        for(var i = 0; i < arrayNames.length; i++){
+            for(var j= 0; j < arrayLastNames.length; j++){
+                if(arrayNames[i].id === arrayLastNames[j].id)
+                    sameIdCount++
+            }
+        }
+    console.log(sameIdCount);
 
-    console.log({firstNameWithId, lastNameWithId})
-  }
-
-  componentDidUpdate(prevProps) {
-    if(prevProps.inputValue !== this.props.inputValue) {
-      this.setState({
-        inputValueInComponent: this.props.inputValue
-      })
-    }
-  }
-
-  render() {
-    const { inputValueInComponent } = this.state
-    const { onChangeInputValue } = this.props
-
+    let sortedArrayNames = [];
+    arrayNames.map((name) => {
+        sortedArrayNames[arrayLastNames.findIndex(surname => surname.id === name.id)] = name.firstName;
+    });
+    sortedArrayNames = sortedArrayNames.filter(v => v).sort()
+    console.log(sortedArrayNames);
+    
+    let sortedArrayLastNames = [];
+    arrayLastNames.map((surname) => {
+        sortedArrayLastNames[arrayNames.findIndex(name => name.id === surname.id)] = surname.lastName;
+    });
+    sortedArrayLastNames = sortedArrayLastNames.filter(v => v).sort()
+    console.log(sortedArrayLastNames);
+    
+    const filteredNames = sortedArrayNames.filter((fullName) => fullName.toLowerCase().includes(inputValueInComponent.toLowerCase()))
+    
+    const filteredLastNames = sortedArrayLastNames.filter((fullName) => fullName.toLowerCase().includes(inputValueInComponent.toLowerCase()))
+    
     return (
-      <input
-        onChange={e => onChangeInputValue(e.target.value)}
-        value={inputValueInComponent}
-      />
+        <>
+            <input
+                onChange={e => props.onChangeInputValue(e.target.value)}
+                value={inputValueInComponent}
+            />
+            <div style={{display: 'flex', position: 'absolute'}}>
+                <div style={{marginRight: '1em'}}>
+                    {inputValueInComponent === '' ? (null) :
+                        filteredNames?.map((name)=>
+                        <p key={name}>{name}</p>
+                    )}
+                </div>
+                <div>
+                    {inputValueInComponent === '' ? (null) :
+                        filteredLastNames?.map(surname=>
+                        <p key={surname}>{surname}</p>
+                    )}
+                </div>
+            </div> 
+        </>
     )
-  }
 }
